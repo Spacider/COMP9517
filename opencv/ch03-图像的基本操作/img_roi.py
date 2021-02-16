d171e367c84f0792fe7d2238edfaed1da62d2991
Spacider <gary_stu@163.com>
2021-02-16 14:45:01 +1100

    ch03(end)

--- opencv/ch03-图像的基本操作/img_roi.py
+++ opencv/ch03-图像的基本操作/img_roi.py
@@ -5,6 +5,7 @@ __author__ = 'Gary'
 from typing import List
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 img = cv2.imread('../pic/messi5.jpg')
 
@@ -36,3 +37,26 @@ print(img[100, 100])
 print(img.item(10, 10, 2))
 img.itemset((10, 10, 2), 100)
 print(img.item(10, 10, 2))
+
+'''
+创立边界
+'''
+BLUE = [255, 255 , 0]
+replicate = cv2.copyMakeBorder(img, top=10, bottom=10, left=10, right=10, borderType=cv2.BORDER_REPLICATE)
+
+reflect = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_REFLECT)
+reflect101 = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_REFLECT_101)
+wrap = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_WRAP)
+
+constant = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=BLUE)  # value 边界颜色
+
+plt.subplot(231), plt.imshow(img, 'gray'), plt.title('ORIGINAL')
+plt.subplot(232), plt.imshow(replicate, 'gray'), plt.title('REPLICATE')
+plt.subplot(233), plt.imshow(reflect, 'gray'), plt.title('REFLECT')
+plt.subplot(234), plt.imshow(reflect101, 'gray'), plt.title('REFLECT_101')
+plt.subplot(235), plt.imshow(wrap, 'gray'), plt.title('WRAP')
+plt.subplot(236), plt.imshow(constant, 'gray'), plt.title('CONSTANT')
+
+plt.show()
+
+
