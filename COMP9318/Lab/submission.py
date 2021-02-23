dfebd90ed807291090db11c560153a45b5fdceb0
Spacider <gary_stu@163.com>
2021-02-23 14:11:54 +1100

    add lab01

--- COMP9318/Lab/submission.py
+++ COMP9318/Lab/submission.py
@@ -9,7 +9,11 @@ def add(a, b): # do not change the heading of the function
 ################# Question 1 #################
 
 def nsqrt(x): # do not change the heading of the function
-    pass # **replace** this line with your code
+    for n in range(x + 1):
+        if n * n == x:
+            return n
+        elif n * n > x:
+            return n - 1
 
 
 ################# Question 2 #################
@@ -22,7 +26,14 @@ def nsqrt(x): # do not change the heading of the function
 ## NOTE: you must use the default values of the above parameters, do not change them
 
 def find_root(f, fprime, x_0=1.0, EPSILON = 1E-7, MAX_ITER = 1000): # do not change the heading of the function
-    pass # **replace** this line with your code
+    current_iter = 1
+    while current_iter < MAX_ITER:
+        x_new = x_0 - f(x_0) / fprime(x_0)
+        if abs(x_0 - x_new) < EPSILON:
+            return x_new
+        x_0 = x_new
+        current_iter += 1
+    return x_0
 
 
 ################# Question 3 #################
@@ -41,7 +52,49 @@ class Tree(object):
         self.children.append(node)
 
 def make_tree(tokens): # do not change the heading of the function
-    pass # **replace** this line with your code    
+    num = 0
+    t = None
+    while num < len(tokens) - 1:
+        if tokens[num + 1] == '[':
+            tmp_tree, num = need_new_tree(tokens, num)
+        else:
+            tmp_tree = Tree(tokens[num])
+        if t is None:
+            t = tmp_tree
+        else:
+            t.add_child(tmp_tree)
 
-def max_depth(root): # do not change the heading of the function
-    pass # **replace** this line with your code
+    return t
+
+
+def need_new_tree(tokens, num):
+    new_tree = Tree(tokens[num])
+    num = num + 2
+    while tokens[num] != ']':
+        if tokens[num + 1] == '[':
+            temp_tree, num = need_new_tree(tokens, num)
+            new_tree.add_child(temp_tree)
+        else:
+            new_tree.add_child(Tree(tokens[num]))
+        num += 1
+    return new_tree, num
+
+
+def max_depth(root):  # do not change the heading of the function
+    if root is None:
+        return 0
+
+    depth = 1
+    max_depth = 1
+    vector = [(root, depth)]
+    while len(vector) is not 0:
+        tmp = vector.pop()
+        if len(tmp[0].children) > 0:
+            depth = tmp[1] + 1
+            max_depth = max(depth, max_depth)
+            for child in tmp[0].children:
+                vector.append((child, depth))
+
+    return max_depth
+
+
---
+++ COMP9318/Lab/test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+__author__ = 'Gary'
+
+from math import sqrt, floor
+import math
+import COMP9318.Lab.submission as submission
+import COMP9318.Lab.str_to_tokens as str_to_tokens
+
+'''
+Question 01
+'''
+num = 0
+print(floor(sqrt(num)))
+result = submission.nsqrt(num)
+print(result)
+
+'''
+Question 02
+'''
+def f(x):
+    return x * math.log(x) - 16.0
+
+def fprime(x):
+    return 1.0 + math.log(x)
+
+x = submission.find_root(f, fprime)
+print(x)
+print(f(x))
+
+'''
+Question 3
+'''
+
+def print_tree(root, indent=0):
+    print(' ' * indent, root)
+    if len(root.children) > 0:
+        for child in root.children:
+            print_tree(child, indent+4)
+
+str_tree = '''
+1 [2 [3 4       5          ] 
+   6 [7 8 [9]   10 [11 12] ] 
+   13
+  ]
+'''
+toks = str_to_tokens.str_to_tokens(str_tree)
+tt = submission.make_tree(toks)
+print_tree(tt)
+
+depth = submission.max_depth(tt)
+print("depth is ", depth)
+
+
+
