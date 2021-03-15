3d3034df07bc16c5ae4723fcc7aa52247fc00a2e
Gary <gary_stu@163.com>
2021-03-15 18:23:45 +1100

    lab3

--- COMP9318/lab3/submission.py
+++ COMP9318/lab3/submission.py
@@ -11,45 +11,52 @@ def buc_rec_optimized(df):
     # df = helper.project_data(df, 1) 0 选择第一列 1选择第二列
     # df = helper.select_data(df, 0, 1) 选择第一列值为 1 的全部数据
     # df = helper.slice_data_dim0(df, 1) # 选择第1列值为1的剩余数据
+    result_df = pd.DataFrame(data=None, columns=df.columns)
 
-    if len(df) == 1:
+    if len(df) == 1111111111111111:
         # single-tuple optimization
-        result_df = None
-        print("this is single tuple")
+        pass
     else:
         # multiple tuple optimization
-        result_df = pd.DataFrame(data=None, columns=df.columns)
-        dims = df.columns.tolist()
-        dims.remove('M')
-        result_df = multiple_tuple_optimization(df, dims, result_df)
+        result_df = multiple_tuple_optimization(df, result_df)
+    result_df.index = range(len(result_df))
     return result_df
 
 
-def multiple_tuple_optimization(df, dims, result_df):
+def multiple_tuple_optimization(df, result_df):
+    dims = df.shape[1]
     if len(dims) == 0:
         return result_df
     else:
-        current_dim = dims[0]
-        value_of_dim = df[current_dim]
+        current_dim = dims[0]  # 'A'
+        value_of_dim = helper.project_data(df, 0)
         # for each distinct value v in dim1
-        for data in set(value_of_dim):
-            origin_data = helper.select_data(df, 0, data)
-            result_df = result_df.append(origin_data, ignore_index=True)
-            # print(result_df)
-            slice_data = helper.slice_data_dim0(df, data)
-            print("--------------slice data start -----------")
-            print("data = ", data, 'len(slice_data)=', len(slice_data))
-            slice_data.loc['ALL'] = slice_data.sum()
-            print(slice_data)
-            print("--------------slice data end ----------")
 
+        for dim in dims:
+            print("dim:", dim)
+            for data in set(value_of_dim):
 
-            # dims.pop(0)
-            # dict = multiple_tuple_optimization(slice_data, dict)
-    return result_df
+                slice_data = helper.slice_data_dim0(df, data)
+                print(slice_data)
+
+                result_df = multiple_tuple_optimization(slice_data, result_df)
+
+        # else:
+        #     tmp_data = df.copy()
+        #     result_df = processing_data(tmp_data, current_dim, None, result_df)
+        # print("--------------slice data start -----------")
+        # print(slice_data)
+        # print("--------------slice data end ----------")
+        # print("--------------result_df start -----------")
+        # print(result_df)
+        # print("--------------result_df end -----------")
+        # dims.remove(current_dim)
+        # data = df.drop(columns=current_dim)
+        # result_df = multiple_tuple_optimization(data, dims, result_df)
+        return result_df
 
 
 if __name__ == '__main__':
-    input_data = helper.read_data('./asset/a_.txt')
+    input_data = helper.read_data('./asset/c_.txt')
     output = buc_rec_optimized(input_data)
     print(output)
