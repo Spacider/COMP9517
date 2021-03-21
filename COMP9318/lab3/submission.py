152230bcbe6a321eb36a44b8f1278e80a9cd7768
Gary <gary_stu@163.com>
2021-03-21 19:16:20 +1100

    9517 lab4

--- COMP9318/lab3/submission.py
+++ COMP9318/lab3/submission.py
@@ -8,55 +8,81 @@ import helper
 
 def buc_rec_optimized(df):
 
-    # df = helper.project_data(df, 1) 0 选择第一列 1选择第二列
-    # df = helper.select_data(df, 0, 1) 选择第一列值为 1 的全部数据
-    # df = helper.slice_data_dim0(df, 1) # 选择第1列值为1的剩余数据
     result_df = pd.DataFrame(data=None, columns=df.columns)
+    columns = []
 
-    if len(df) == 1111111111111111:
+    if len(df) == 1:
         # single-tuple optimization
-        pass
+        single_tuple_optimization(df, result_df, columns)
     else:
         # multiple tuple optimization
-        result_df = multiple_tuple_optimization(df, result_df)
-    result_df.index = range(len(result_df))
+        result_df = buc(df, result_df, columns)
+    # result_df.index = range(len(result_df))
     return result_df
 
 
-def multiple_tuple_optimization(df, result_df):
-    dims = df.shape[1]
-    if len(dims) == 0:
-        return result_df
+def copy_list(list):
+    new_list = []
+    for i in list:
+        new_list.append(i)
+    return new_list
+
+
+def single_tuple_optimization(df, result_df, columns):
+    all_possible = []
+
+    if len(df.columns) > 1:
+        for index in range(df.shape[1] - 1):
+            tmp_list = []
+            # two possibles 'ALL' or data
+            single_possible = [df.iloc[0, index], 'ALL']
+            if len(all_possible):
+                for tmp in all_possible:
+                    for single in single_possible:
+                        new_tmp = copy_list(tmp)
+                        new_tmp.append(single)
+                        tmp_list.append(new_tmp)
+                # update the all possibles list
+                all_possible = tmp_list
+            else:
+                # if all possibles list is empty
+                # that means first time
+                # need to import first data and 'ALL'
+                all_possible = [[_] for _ in single_possible]
     else:
-        current_dim = dims[0]  # 'A'
-        value_of_dim = helper.project_data(df, 0)
-        # for each distinct value v in dim1
-
-        for dim in dims:
-            print("dim:", dim)
-            for data in set(value_of_dim):
-
-                slice_data = helper.slice_data_dim0(df, data)
-                print(slice_data)
-
-                result_df = multiple_tuple_optimization(slice_data, result_df)
-
-        # else:
-        #     tmp_data = df.copy()
-        #     result_df = processing_data(tmp_data, current_dim, None, result_df)
-        # print("--------------slice data start -----------")
-        # print(slice_data)
-        # print("--------------slice data end ----------")
-        # print("--------------result_df start -----------")
-        # print(result_df)
-        # print("--------------result_df end -----------")
-        # dims.remove(current_dim)
-        # data = df.drop(columns=current_dim)
-        # result_df = multiple_tuple_optimization(data, dims, result_df)
-        return result_df
+        all_possible = [columns]
+        columns = []
+    for possible in all_possible:
+        # add M to the data
+        possible.append(df.iloc[0, df.shape[1] - 1])
+        new_columns = copy_list(columns)
+        new_columns.extend(possible)
+        # add lines to dataframe
+        result_df.loc[len(result_df)] = new_columns
 
 
-if __name__ == '__main__':
-    input_data = helper.read_data('./asset/c_.txt')
-    output = buc_rec_optimized(input_data)
-    print(output)
+def buc(df, result_df, columns):
+    dims = len(df.columns)  # len of dims
+
+    if len(df) == 1:
+        single_tuple_optimization(df, result_df, columns)
+    else:
+        if dims == 1:
+            df_sum = sum(helper.project_data(df, 0))
+            columns.append(df_sum)
+            result_df.loc[len(result_df)] = columns
+        else:
+            dim_values = set(helper.project_data(df, 0))
+            tmp = copy_list(columns)
+            for value in dim_values:
+                tmp_column = copy_list(tmp)
+                slice_data = helper.slice_data_dim0(df, value)
+                tmp_column.append(value)
+                buc(slice_data, result_df, tmp_column)
+
+            sub_data = helper.remove_first_dim(df)
+            tmp_column = copy_list(tmp)
+            tmp_column.append('ALL')
+            buc(sub_data, result_df, tmp_column)
+
+    return result_df
